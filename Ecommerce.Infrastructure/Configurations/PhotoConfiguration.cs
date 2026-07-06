36254568713c5fe4994d81ad9467c50e9e950888
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-07-06 19:52:21 +0300

    Refactor: seed data for photo

---
+++ Ecommerce.Infrastructure/Configurations/PhotoConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Data.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Configurations
+{
+    public class PhotoConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.HasData(
+                new Photo { Id = 1, ImageName = "Image 1", ProductId = 1 }
+            );
+        }
+    }
+}
---
+++ Ecommerce.Infrastructure/Migrations/20260706164934_seedphotodata1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedphotodata1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Photos",
+                columns: new[] { "Id", "ImageName", "ProductId" },
+                values: new object[] { 1, "Image 1", 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
