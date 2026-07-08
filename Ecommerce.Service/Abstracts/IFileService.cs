d2677908319aed2ecc02d40fed27c3ea89531b95
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-07-08 13:27:13 +0300

    Feat: create File Service for add collection of product photos

---
+++ Ecommerce.Service/Abstracts/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ecommerce.Service.Abstracts
+{
+    public interface IFileService
+    {
+        public Task<bool> IsValidImageFileMaxLength(IFormFile file);
+        public Task<bool> IsValidImageFileExtension(IFormFile file);
+        public Task<List<string>> UploadImage(string Location, IFormFileCollection file);
+        public void DeleteImage(string Location, string fileName);
+    }
+}
