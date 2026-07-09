1fbe0d021f99113e8c02cc07c40e8e1a5b068f8c
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-07-09 18:10:29 +0300

    Feat:add Nav-bar Component

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './Core/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  title = signal('Client');
+  title = signal('EcommerceClient');
 }
